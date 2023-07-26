3158e20104c002c5ad7973af8e3370d1f3368fa8
nikhilpakhloo <nikhilpakhloo@gmail.com>
2023-07-26 14:41:08 +0530

    my weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import HotBg from './assets/hot.jpg'
+
+
+import { useEffect, useState } from 'react';
+import SnowBg from './assets/snow.jpg'
+import Descriptions from './components/Descriptions';
+import { getFormattedWeatherData } from './weatherService';
+import Sunnybg from './assets/sun.jpg'
+
+
+
 
 function App() {
+  const [city,setCity]=useState("Himachal Pradesh")
+  const [weather,setWeather] = useState(null)
+  const [units, setUnits] = useState("metric")
+  const [bg, setBg] = useState(SnowBg)
+  useEffect(()=>{
+    const fetchWeatherData = async()=>{
+      const data = await getFormattedWeatherData(city, units)
+      setWeather(data)
+      
+
+      const threshold = units ==='metric'? 20:60;
+      if (data.cel<= threshold) setBg(SnowBg)
+      else setBg(Sunnybg) 
+      
+
+    };
+    fetchWeatherData()
+   
+  },[units,city]);
+  // const handleUnitsClick=(e)=>{
+  //   const button = e.currentTarget;
+  //   const currentUnit = button.innerText.slice(1);
+  //   const isCelcius = currentUnit ==='C';
+  //   button.innerText = isCelcius? '°F':'°C';
+  //   setUnits(isCelcius ? "metric":"imperial")
+
+  // }
+  const enterKeyPressed =(e)=>{
+    if (e.keyCode ===13){
+      setCity(e.currentTarget.value)
+      e.currentTarget.blur();
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundImage:`url(${bg})`}}>
+    <div className="overlay">
+    
+      {
+        
+        weather && (
+          
+          <div className="container">
+          <div className="section section__inputs">
+            <input onKeyDownCapture={enterKeyPressed} type="text" placeholder='Enter City...' name='city' />
+            
+          </div>
+         <div className="section section__temperature">
+            <div className="icon">
+              <h3>{weather.name},{weather.country}</h3>
+              <img src={weather.iconURL} alt="weatherIcon" />
+              <h3>{weather.description}</h3> 
+            </div>
+            <div className='temperature'>
+              <h1>{weather.cel.toFixed()}°C</h1>
+            </div>
+          </div>
+          <Descriptions weather = {weather} units={units}/>
+        </div>
+          
+        )
+      }
+     
+    </div>
     </div>
   );
 }
---
+++ src/weatherService.js
@@ -0,0 +1,63 @@
+const API_KEY = "374707e791cb597e566d4e941c8378af";
+
+const makeIconURL = (iconID) =>
+  `https://openweathermap.org/img/wn/${iconID}@2x.png`;
+  
+const getFormattedWeatherData = async (city, units ) => {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}}`;
+  const data = await fetch(URL)
+    .then((res) => res.json())
+    .then((data) => data);
+
+
+  
+
+  const {
+    weather,
+    main: { temp, feels_like, temp_max, temp_min, pressure, humidity },
+    wind: { speed },
+    sys: { country },
+    name,
+  } = data;
+  console.log(data);
+  const { description, icon } = weather[0];
+  
+
+
+  const kelvinToCel=(kelvin)=>{
+    return kelvin -273.15;
+
+  }
+   const tempinKelvin = temp
+   const mininKelvin=temp_min
+   const maxinKelvin=temp_max
+   const feelinKelvin = feels_like
+   const cel = kelvinToCel(tempinKelvin);
+   const mincel = kelvinToCel(mininKelvin)
+   const maxcel = kelvinToCel(maxinKelvin)
+
+   const feelcel = kelvinToCel(feelinKelvin)
+
+   
+
+  //  console.log(cel  )
+
+
+  return {
+    description,
+    iconURL: makeIconURL(icon),
+    cel,
+    weather,
+    feelcel,
+    maxcel,
+    mincel,
+    pressure,
+    humidity,
+    
+    speed,
+    country,
+    name,
+   
+  };
+};
+export { getFormattedWeatherData };
